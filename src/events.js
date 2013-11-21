8a11fb2e368efcf93fdc43d8ce248fffa64718cf
Clément 'birdy' Danjou <clement.danjou@honeyvinegar.fr>
2013-11-21 09:30:45 +0100

    Adding events

---
+++ src/events.js
@@ -0,0 +1,24 @@
+var bindEvents = function(tiles) {
+    var mousemove = function(e) {
+        tiles.mousemoveListener(tiles, e);
+    }
+
+    var mousewheel = function(e) {
+        tiles.mousewheelListener(tiles, e);
+    }
+
+    mainContainer.addEventListener('mousedown', function(e) {
+        e.stopPropagation();
+        dragEvent = e;
+        console.log(this);
+        this.addEventListener('mousemove', mousemove);
+    });
+
+    mainContainer.addEventListener('mouseup', function() {
+        dragEvent = undefined;
+        this.removeEventListener('mousemove', mousemove);
+    });
+
+    mainContainer.addEventListener('mousewheel', mousewheel);
+    mainContainer.addEventListener('wheel', mousewheel);
+};
