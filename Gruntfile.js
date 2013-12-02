69758b913957f8cec84a6350395dec21514d6bf3
Clément 'birdy' Danjou <clement.danjou@honeyvinegar.fr>
2013-12-02 23:36:56 +0100

    AUto exec tests on compile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,39 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    smash: {
-        build: {
-            src: 'src/<%= pkg.name %>.js',
-            dest: 'build/<%= pkg.name %>.js'
-        }
-    },
-    uglify: {
-      options: {
-          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-          src: 'build/<%= pkg.name %>.js',
-          dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
-    watch: {
-        files: ['src/*.js'],
-        tasks: ['build']
-    }
-  });
+    // Project configuration.
+    grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+	qunit: {
+	    all: ['test/*.html']
+	},
+	smash: {
+            build: {
+		src: 'src/<%= pkg.name %>.js',
+		dest: 'build/<%= pkg.name %>.js'
+            }
+	},
+	uglify: {
+	    options: {
+		banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+	    },
+	    build: {
+		src: 'build/<%= pkg.name %>.js',
+		dest: 'build/<%= pkg.name %>.min.js'
+	    }
+	},
+	watch: {
+            files: ['src/*.js'],
+            tasks: ['build']
+	}
+    });
 
-  grunt.loadNpmTasks('grunt-smash');
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-smash');
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task(s).
-  grunt.registerTask('build', ['smash:build','uglify:build']);
+    // Default task(s).
+    grunt.registerTask('build', ['smash:build','uglify:build', 'qunit:all']);
 
 };
